6686ab50fcccbab92532e20a28ff0af48b8e1f2a
valerdos <v.sveyko@gmail.com>
2019-07-02 22:59:27 +0200

    tmp error handle

--- Sveyko.B1RADIO/Startup.cs
+++ Sveyko.B1RADIO/Startup.cs
@@ -49,11 +49,11 @@ namespace Sveyko.B1RADIO
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
             {
                 app.UseExceptionHandler("/Soundtracks/Error");
             }
